13ffa1ad00ddaf1b08cfacde4b95022c712552fe
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2016-11-07 23:36:32 +0100

    Reorganized the code into src & dist folders.

--- gulpfile.js
+++ gulpfile.js
@@ -10,18 +10,18 @@ gulp.task('js', function() {
   var files = ['worker.js', 'index.js'];
 
   var tasks = files.map(function(file) {
-    return browserify('./src/' + file)
+    return browserify('./src/js/' + file)
       .transform(babelify, { presets: ['es2015', 'react'] })
       .bundle()
       .pipe(source(file))
-      .pipe(gulp.dest('./build'))
+      .pipe(gulp.dest('./dist/js'))
   });
 
   es.merge.apply(null, tasks);
 });
 
 gulp.task('webserver', ['js'], function() {
-  connect.server();
+  connect.server({root: 'dist'});
 });
 
 gulp.task('default', ['webserver']);
