964d5b41338603f77780ada677e9728fa23f0d27
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2016-11-09 00:12:37 +0100

    publish: Added the deploy script from https://github.com/X1011/git-directory-deploy.
    generated from commit 05058f9f9ef3312c38a132c12747c796feeba63b

--- sw.js
+++ sw.js
@@ -1,25 +1,19 @@
-self.addEventListener('install', function(event) {
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+self.addEventListener('install', function (event) {
   console.log('installing my first service worker');
 
-  event.waitUntil(
-    caches.open('online-metronome-v3').then(function(cache) {
-      return cache.addAll([
-        'style/style.css',
-        'style/bootstrap.min.css',
-        'build/index.js',
-        'build/worker.js',
-        'https://piotr-szachewicz.github.io/online-metronome/'
-      ]);
-    })
-  );
+  event.waitUntil(caches.open('online-metronome-v3').then(function (cache) {
+    return cache.addAll(['/', 'style/style.css', 'style/bootstrap.min.css', 'build/index.js', 'build/worker.js']);
+  }));
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   console.log("fetching " + event.request.url);
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then(function (response) {
+    return response || fetch(event.request);
+  }));
 });
 
+},{}]},{},[1]);
